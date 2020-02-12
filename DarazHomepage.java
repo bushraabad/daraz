fccc626f46ac6a38e425f9d00f5acae14d51020b
bushraabad <59051758+bushraabad@users.noreply.github.com>
2020-02-12 13:03:43 +0500

    Add files via upload

---
+++ DarazHomepage.java
@@ -0,0 +1,18 @@
+package darazhomepage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DarazHomepage {
+	
+	private static WebElement element = null;
+	public static Object login;
+	
+	public static WebElement login(WebDriver driver){
+		
+		element = driver.findElement(By.id("anonLogin"));
+		return element;
+		
+	}
+}
